6b61b3f9ed50ed45e264533d702e19852efb38af
javiercolochoConsystec <javier.colocho@consystec-corp.com>
2025-11-17 23:02:08 -0600

    Update EditTemplateForm.jsx

--- src/components/EditTemplateForm.jsx
+++ src/components/EditTemplateForm.jsx
@@ -396,6 +396,16 @@ const EditTemplateForm = () => {
 
     }
 
+    if (!isValid) {
+    Swal.fire({
+      title: 'Error',
+      text: 'Campos incompletos.',
+      icon: 'error',
+      confirmButtonText: 'Cerrar',
+      confirmButtonColor: '#00c3ff'
+    });
+  }
+
 
     return isValid; // Retornar el valor final de isValid
   };
@@ -406,13 +416,7 @@ const EditTemplateForm = () => {
 
     const isValid = validateFields();
     if (!isValid) {
-      Swal.fire({
-        title: 'Error',
-        text: 'Campos incompletos.',
-        icon: 'error',
-        confirmButtonText: 'Cerrar',
-        confirmButtonColor: '#00c3ff'
-      });
+      //Swal.fire({title: 'Error', text: 'Campos incompletos.', icon: 'error', confirmButtonText: 'Cerrar', confirmButtonColor: '#00c3ff'});
       setLoading(false);
       return;
     }
